187bc30904288ce9fdf5cf17bc4c614183c5a31e
Jonathan Yu <jawnsy@cpan.org>
2010-10-03 10:28:04 -0400

    Add a component to validate metadata validate.c provides several useful functions for validating package metadata. It currently provides a single function for validating package names.

---
+++ include/debctrl/validate.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+** The libdebctrl library, this package and its contents (unless explicitly
+** noted otherwise) are licensed under the MIT/X11 License (see the included
+** LICENSE file for copyright information, full terms and conditions).
+****************************************************************************/
+
+/** \file
+ * Debian package information validation
+ * \author Jonathan Yu <jawnsy@cpan.org>
+ *
+ * For more details on how this works, see \ref validate.c
+ */
+
+#ifndef DEBCTRL_VALIDATE_H
+#define DEBCTRL_VALIDATE_H
+
+#include <debctrl/common.h>
+
+int dc_valid_package(
+  const char *name
+);
+
+#endif /* DEBCTRL_VALIDATE_H */
---
+++ src/validate.c
@@ -0,0 +1,87 @@
+/***************************************************************************
+** The libdebctrl library, this package and its contents (unless explicitly
+** noted otherwise) are licensed under the MIT/X11 License (see the included
+** LICENSE file for copyright information, full terms and conditions).
+****************************************************************************/
+
+/** \file
+ * Debian package information validation
+ * \author Jonathan Yu <jawnsy@cpan.org>
+ *
+ * This component provides validation for Debian package information, such as
+ * package and dependency names, which are common to various types of control
+ * files.
+ *
+ * \bug All error messages are in English and are not internationalized
+ *
+ * \see "Control files and their fields", from the Debian Policy Manual:
+ * http://www.debian.org/doc/debian-policy/ch-controlfields.html
+ */
+
+#include <ctype.h>    /* for: various isalpha/etc functions */
+
+#include <debctrl/validate.h>
+
+/**
+ * Validate a package name
+ *
+ * Given a string containing the name of a Debian source or binary package,
+ * this will determine whether the package name meets the specification set
+ * in Debian Policy 5.6.1.
+ *
+ * Debian Policy stipulates that:
+ * - Package names must be at least two characters long
+ * - Package names must begin with a lowercase alphabetic or a numeric
+ *   character
+ * - Package names must contain only lowercase alphabetic, numeric, or
+ *   '+', '-' and '.' characters
+ *
+ * \param[in] name The name of a package to validate
+ *
+ * \retval 0 if the package name is invalid
+ * \retval 1 if the package name appears valid
+ *
+ * \see "5.6.1 Source" (and "5.6.7 Package"), from the Debian Policy Manual:
+ * http://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-Source
+ */
+int dc_valid_package(
+  const char *name
+) {
+  /* Validate the package name, according to Policy 5.6.1 */
+
+  /* Package names must be at least two characters long */
+  if (name == NULL || *name == '\0' || *(name+1) == '\0')
+  {
+    return 0;
+  }
+
+  /* Package names must begin with a lowercase alphabetic or numeric
+   * character.
+   */
+  if (!(
+    islower(*name) ||
+    isdigit(*name)
+  )) {
+    return 0;
+  }
+  name++;
+
+  /* Package names must contain only lowercase alphabetic, numeric, or '+',
+   * '-' and '.' characters.
+   */
+  while (*name != '\0')
+  {
+    if (!(
+      islower(*name) ||
+      isdigit(*name) ||
+      *name == '+' ||
+      *name == '-' ||
+      *name == '.'
+    )) {
+      return 0;
+    }
+    name++;
+  }
+
+  return 1;
+}
